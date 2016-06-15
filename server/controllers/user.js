11d73ab26d47622414ae8d4e376b8ea019840147
msequino <mansequino@gmail.com>
2016-06-15 12:29:13 +0200

    added file upgrading

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -92,11 +92,5 @@ module.exports.updateUser = function(req,res,next){
 
 
 module.exports.sendApk = function(req,res,next){
-  res.download('apps/hucare/server/apk/app-debug.apk', 'newApp.apk', function(err){
-    console.log(err);
-    if (err)
-      log.log('error',err);
-    else
-      log.log('info',"download done");
-  });
+  res.sendFile('apps/hucare/server/apk/app-debug.apk', 'newApp.apk');
 }
