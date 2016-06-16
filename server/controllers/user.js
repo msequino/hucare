fba699a7fd600c63bbfe63037bd2dd3d79a15707
msequino <mansequino@gmail.com>
2016-06-16 17:35:13 +0200

    added simple git

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -104,7 +104,7 @@ module.exports.deploy = function(req,res,next){
     if(req.body.pusher.hasOwnProperty('name')){
       if(req.body.pusher.name == 'msequino'){
         console.log("MAKE PULL");
-        console.log(__dirname + '/../../');
+        console.log(__dirname + '\\..\\..');
         require('simple-git')(__dirname + '\\..\\..\\').pull(function(err) {
           console.log(err);
           res.end();
